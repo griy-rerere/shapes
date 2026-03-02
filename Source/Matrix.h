d35acaa32c6916648bb7e348a97b553afcc8269f
Zer0Dash <rage09274@gmail.com>
2026-03-02 14:56:09 +0300

    Update Matrix.h

--- Source/Matrix.h
+++ Source/Matrix.h
@@ -1,1 +1,26 @@
+#include <stddef.h>
 
+class Matrix {
+    bool **array_;
+    size_t cols_;
+    size_t rows_;
+public:
+    Matrix(size_t height, size_t width): rows_(height), cols_(width)
+    {
+        array_ = new bool*[height];
+        for (int i = 0; i < height; i++)
+        {
+            array_[i] = new bool[width];
+        }
+    }
+    ~Matrix()
+    {
+        for (int i = 0; i < rows_; i++)
+        {
+            delete[] array_[i];
+        }
+        delete[] array_;
+    }
+
+    bool get(size_t row, size_t col)
+};
